4e3098a1c351cc5c00556ac83e5cb4b0815180eb
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-03-04 04:34:52 -0700

    Create user service, not implemented.

---
+++ dotnet6-auth-sqlite-api/Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using dotnet6_auth_sqlite_api.Contracts;
+using dotnet6_auth_sqlite_api.Models.Authentication;
+
+namespace dotnet6_auth_sqlite_api.Services
+{
+    public class UserService : IUserService
+    {
+        public User Create(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(UserLogin userLogin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
