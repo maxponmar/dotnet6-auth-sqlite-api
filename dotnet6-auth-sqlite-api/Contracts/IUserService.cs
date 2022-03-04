6ecdc2bedcf0c4cc54f553ec52c13d143e4748ee
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-03-04 04:32:38 -0700

    Create user service contract.

---
+++ dotnet6-auth-sqlite-api/Contracts/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using dotnet6_auth_sqlite_api.Models.Authentication;
+
+namespace dotnet6_auth_sqlite_api.Contracts
+{
+    public interface IUserService
+    {
+        public User Get(UserLogin userLogin);
+        public User Create(User user);
+        public User Update(User user);
+        public bool Delete(int userId);
+    }
+}
