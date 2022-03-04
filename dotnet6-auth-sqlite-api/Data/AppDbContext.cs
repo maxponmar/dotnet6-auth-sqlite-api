f2a64922c5c4a6b226e6f5a122389620852ddbad
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-03-04 04:28:18 -0700

    Create AppDbContext.
    -TODO: Separate modelBuilder configuration for each model.

---
+++ dotnet6-auth-sqlite-api/Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using dotnet6_auth_sqlite_api.Models.Authentication;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace dotnet6_auth_sqlite_api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(connectionString: "Filename=AppB.db",
+                sqliteOptionsAction: op =>
+                {
+                    op.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+                });
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
