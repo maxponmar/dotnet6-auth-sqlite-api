4b3e34554e667b924f1c7fe23a95c4c5c3101426
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-03-04 04:20:51 -0700

    Create user login DTO.

---
+++ dotnet6-auth-sqlite-api/Models/Authentication/UserLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace dotnet6_auth_sqlite_api.Models.Authentication
+{
+    public class UserLogin
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
