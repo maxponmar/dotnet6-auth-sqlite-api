c7aa18e168605b2fd76e7755e642e3d8624dd506
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-03-04 18:40:42 -0700

    Add jwt settings.

--- dotnet6-auth-sqlite-api/Program.cs
+++ dotnet6-auth-sqlite-api/Program.cs
@@ -1,5 +1,6 @@
 
 using dotnet6_auth_sqlite_api.Data;
+using Microsoft.OpenApi.Models;
 
 // Create Database if not exists
 //using (var db = new AppDbContext())
@@ -15,7 +16,33 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Description = "Bearer Authentication with JWT Token",
+        Type = SecuritySchemeType.Http
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            new List<string>()
+        }
+    });
+});
+
 builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
