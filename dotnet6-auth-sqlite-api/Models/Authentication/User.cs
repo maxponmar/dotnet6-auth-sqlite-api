09098bcf0812cfae5e8db70acb2fc4d045d7f1ce
Maximiliano Ponce Marquez <maxponce.marquez@outlook.com>
2022-03-05 06:00:36 -0700

    Add required annotation.

--- dotnet6-auth-sqlite-api/Models/Authentication/User.cs
+++ dotnet6-auth-sqlite-api/Models/Authentication/User.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace dotnet6_auth_sqlite_api.Models.Authentication
 {
@@ -6,8 +7,11 @@ namespace dotnet6_auth_sqlite_api.Models.Authentication
     {
         [JsonIgnore]
         public int Id { get; set; }
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public string Role { get; set; }
     }
 }
--- dotnet6-auth-sqlite-api/Models/Authentication/UserLogin.cs
+++ dotnet6-auth-sqlite-api/Models/Authentication/UserLogin.cs
@@ -1,8 +1,12 @@
-﻿namespace dotnet6_auth_sqlite_api.Models.Authentication
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotnet6_auth_sqlite_api.Models.Authentication
 {
     public class UserLogin
     {
+        [Required]
         public string UserName { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
